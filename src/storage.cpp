6a526be48e35755b96f64ffc5bcd99f9289fa21a
a4z <harald.achitz@gmail.com>
2021-04-21 19:32:46 +0200

    add remove function

--- src/storage.cpp
+++ src/storage.cpp
@@ -4,50 +4,50 @@
 #include "cpplib/djinni/cpp/Data.hpp"
 #include "cpplib/djinni/cpp/Storage.hpp"
 
-#include <set>
-#include <optional>
-#include <limits>
 #include <cassert>
+#include <limits>
+#include <optional>
+#include <set>
 namespace cpplib {
 
-    class Storage : public djinni::Storage {
+class Storage : public djinni::Storage {
+  using data_set = std::set<djinni::Data,
+                            bool (*)(const djinni::Data&, const djinni::Data&)>;
 
-        using data_set = std::set<djinni::Data, bool (*)(const djinni::Data&, const djinni::Data&)> ;
+  data_set store{
+      [](const djinni::Data& a, const djinni::Data& b) { return a.id < b.id; }};
 
-        data_set store{ [](const djinni::Data& a, const djinni::Data& b) { return a.id < b.id ;} } ;
+ public:
+  ~Storage() = default;
 
-    public:
-        ~Storage() = default ;
-
-        std::optional<djinni::Data> get(int64_t id) const override{
-            auto find_iter = store.find(djinni::Data{id, ""});
-            if (find_iter != store.end()) {
-                return *find_iter;
-            }
-            return {};
-        }
-
-        bool add(const djinni::Data & data) override {
-            auto insert_info = store.insert(data);
-            return insert_info.second ;
-        }
-
-        int64_t size() const override{
-            assert(store.size() < static_cast<std::size_t>(std::numeric_limits<int64_t>::max()));
-
-            return static_cast<int64_t>(store.size());
-        }
-
-    };
-} // namespace cpplib
-
-
-
-namespace cpplib::djinni
-{
-    std::shared_ptr<Storage>  Storage::create() {
-        return std::make_shared<cpplib::Storage>() ;
+  std::optional<djinni::Data> get(int64_t id) const override {
+    auto find_iter = store.find(djinni::Data{id, ""});
+    if (find_iter != store.end()) {
+      return *find_iter;
     }
-} // namespace cpplib::djinni
+    return {};
+  }
 
+  bool add(const djinni::Data& data) override {
+    auto insert_info = store.insert(data);
+    return insert_info.second;
+  }
 
+  bool remove(int64_t id) override {
+      return store.erase(djinni::Data{id,""}) == 1;
+  }
+
+  int64_t size() const override {
+    assert(store.size() <
+           static_cast<std::size_t>(std::numeric_limits<int64_t>::max()));
+
+    return static_cast<int64_t>(store.size());
+  }
+};
+}  // namespace cpplib
+
+namespace cpplib::djinni {
+std::shared_ptr<Storage> Storage::create() {
+  return std::make_shared<cpplib::Storage>();
+}
+}  // namespace cpplib::djinni
--- tests/cpp/storagecpp_test.cpp
+++ tests/cpp/storagecpp_test.cpp
@@ -1,30 +1,37 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
-#include "cpplib/djinni/cpp/Storage.hpp"
 #include "cpplib/djinni/cpp/Data.hpp"
+#include "cpplib/djinni/cpp/Storage.hpp"
 
 SCENARIO("Using djinni storage from cpp") {
+  auto store = cpplib::djinni::Storage::create();
+  REQUIRE(store != nullptr);
 
-    auto store = cpplib::djinni::Storage::create();
-    REQUIRE(store != nullptr);
+  GIVEN("an empty storage instance and a Data element") {
+    using Data = cpplib::djinni::Data;
+    CHECK_EQ(store->size(), 0);
 
-    GIVEN("an empty storage instance and a Data element") {
-        using Data = cpplib::djinni::Data ;
+    WHEN("adding a element to the empty storage") {
+      Data d{1, "Hello"};
+      auto add_succeeded = store->add(d);
+      THEN("the add operation succeeded") {
+        REQUIRE(add_succeeded);
+        CHECK_EQ(store->size(), 1);
+      }
+      AND_THEN("the same element can not be added again") {
+        REQUIRE_FALSE(store->add(d));
+        CHECK_EQ(store->size(), 1);
+      }
+      AND_THEN("a different element can be added") {
+        REQUIRE(store->add(Data{2, "World"}));
+        CHECK_EQ(store->size(), 2);
+      }
+      AND_THEN("the element can be removed once") {
+        REQUIRE(store->remove(1));
         CHECK_EQ(store->size(), 0);
-        WHEN("adding a element to the empty storage") {
-            Data d{1,"Hello"} ;
-            auto add_succeeded = store->add(d) ;
-            THEN("the add operation succeeded") {
-                REQUIRE(add_succeeded);
-            }
-            AND_THEN("the same element can not be added again") {
-                REQUIRE_FALSE(store->add(d));
-            }
-            AND_THEN("a different element can be added") {
-                REQUIRE(store->add(Data{2,"World"}));
-                CHECK_EQ(store->size(), 2);
-            }
-        }
+        REQUIRE_FALSE(store->remove(1));
+      }
     }
+  }
 }
