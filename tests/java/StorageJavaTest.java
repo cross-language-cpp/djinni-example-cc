feefe8b6db5be6758d688749b0840ee014f58c2f
a4z <harald.achitz@gmail.com>
2021-04-27 20:21:47 +0200

    Add Java test

--- tests/java/StorageJavaTest.java
+++ tests/java/StorageJavaTest.java
@@ -8,32 +8,38 @@ import dev.xlcpp.djinni.hello.Storage;
 import dev.xlcpp.djinni.hello.Data;
 
 
+
+
+//yes, not a beautiful Java JUnit test,
+// just demonstrate basic usage of how to use the djinni generated files
+// and the native lib
 public class StorageJavaTest {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            System.out.println("here:" + args[0]);
+            // assume that is the native lib to load
+            System.out.println("Loading:" + args[0]);
+            try {
+                System.load(args[0]);
+            } catch (Exception e) {
+                //TODO: TODO , make more informative
+                System.exit(2);
+            }
+        } else {
+            // TODO check some locations ...
         }
 
-
-        try {
-            System.out.println("Working Directory = " + System.getProperty("user.dir"));
-
-            var file_path = "/Users/a4z/work/xlcpp/intro/hello_djinni/build/lib/DjinniStorage.framework/DjinniStorage" ;
-            System.load(file_path);
-            // System.setProperty( "java.library.path", "/Users/a4z/work/xlcpp/intro/hello_djinni/build/lib" );
-            // System.loadLibrary("DjinniStorage");
-
-            var d = new Data(1, "Foo") ;
-
-            System.out.println("" + d.getId() + " " + d.getValue() );
-
-        } catch (Exception e) {
-            //TODO: handle
-            System.exit(2);
-
-        }
-
-
+        var store = Storage.create();
+        var d = new Data(1, "Foo") ;
+        assert store.add(d);
+        assert store.size() == 1 ;
+        assert !store.add(d);
+        assert store.add(new Data(2, "Bar"));
+        assert store.size() == 2 ;
+        assert store.remove(1);
+        assert store.size() == 1 ;
+        assert !store.remove(1);
+        assert store.size() == 1 ;
+        System.out.println("all done");
     }
 }
